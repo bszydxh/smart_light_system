13ea64a5de312577155f890d548db47ebb6e6eef
bszydxh <79249935+bszydxh@users.noreply.github.com>
2022-03-13 17:59:59 +0800

    Add files via upload

---
+++ src/password.cpp
@@ -0,0 +1,7 @@
+
+//下面是和风天气的api,api的key手动再申请罢,一天3000次
+#define URL "https://devapi.qweather.com/v7/weather/now?location=108.8325,34.1283&key=yourkey&gzip=n"
+#define URL2 "https://devapi.qweather.com/v7/air/now?location=108.8325,34.1283&key=yourkey&gzip=n"
+#define SSID u8"ssid"
+#define PASSWORD "password"
+#define AUTH_KEY "yourblinkerkey"
